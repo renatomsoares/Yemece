bf1c71a3ac1a3ecae7584803d0a9eb79e754d329
Renato Mesquita <rmsnatal@gmail.com>
2019-04-02 10:11:57 -0300

    General adjustments.

--- app/src/main/java/com/example/yemece/activity/EditImcActivity.java
+++ app/src/main/java/com/example/yemece/activity/EditImcActivity.java
@@ -2,6 +2,7 @@ package com.example.yemece.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 import com.example.yemece.R;
 import com.example.yemece.data.Imc;
 import com.example.yemece.data.ImcDAO;
+import com.example.yemece.enums.GravidadeIndiceImc;
 import com.example.yemece.helpers.CalculoImcHelper;
 
 import java.text.SimpleDateFormat;
@@ -53,13 +55,23 @@ public class EditImcActivity extends Activity {
         CalculoImcHelper calculoImc = new CalculoImcHelper(peso, altura);
 
         String msg;
+        String situacao = ". Parabéns! Você está no peso ideal.";
+
+
+        if (calculoImc.getGravidadeIndice() != GravidadeIndiceImc.NENHUMA) {
+            if (calculoImc.getIndice() > 24.9) {
+                situacao = ". Você precisa perder " + String.format("%.2f", Math.abs(calculoImc.getDiferencaPesoParaSituacaoIdeal())) + "kg para atingir o IMC ideal.";
+            } else {
+                situacao = ". Você precisa ganhar " + String.format("%.2f", Math.abs(calculoImc.getDiferencaPesoParaSituacaoIdeal())) + "kg para atingir o IMC ideal.";
+            }
+        }
+        msg = "IMC registrado em = " + imc.getDataRegistro() + situacao;
 
         if (imc == null) {
 
             Date hoje = new Date();
             Imc imc = new Imc(calculoImc.getSituacao(), peso, altura, new SimpleDateFormat("dd-MM-yyyy (hh:mm)").format(hoje));
             imcDAO.save(imc);
-            msg = "IMC gravado com ID = " + imc.getId();
 
         } else {
             imc.setSituacao(calculoImc.getSituacao());
@@ -68,10 +80,21 @@ public class EditImcActivity extends Activity {
             // A atualização mantém a data de registro.
 
             imcDAO.update(imc);
-            msg = "IMC atualizado com ID = " + imc.getId();
+            msg = "O IMC registrado em = " + imc.getDataRegistro() + " foi atualizado" +  situacao;
         }
 
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        final Toast toast = Toast.makeText(this, msg,Toast.LENGTH_SHORT);
+        toast.show();
+
+        new CountDownTimer(5000, 1000)
+        {
+
+            public void onTick(long millisUntilFinished) {toast.show();}
+            public void onFinish() {toast.show();}
+
+        }.start();
+
+        //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         setResult(RESULT_OK);
         finish();
     }
--- app/src/main/java/com/example/yemece/helpers/CalculoImcHelper.java
+++ app/src/main/java/com/example/yemece/helpers/CalculoImcHelper.java
@@ -4,30 +4,38 @@ import com.example.yemece.enums.GravidadeIndiceImc;
 
 public class CalculoImcHelper {
 
-    private final double limiteAbaixoPeso = 18.5;
-    private static double limiteIdeal = 24.9;
+    private final double limiteMinimoIdeal = 18.5;
+    private static double limiteMaximoIdeal = 24.9;
     private static double limiteLevementeAcimaPeso = 29.9;
     private static double limiteObedisadeGrauI = 34.9;
     private static double limiteObedisadeGrauII = 39.9;
 
 
     private double indice;
+    private double peso;
+    private double altura;
 
     public CalculoImcHelper(double peso, double altura) {
-        this.indice = peso/(altura * 2);
+        this.indice = peso/(altura*altura);
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public double getIndice () {
+        return indice;
     }
 
     public String getSituacao() {
 
-        if (indice < limiteAbaixoPeso) {
+        if (indice < limiteMinimoIdeal) {
             return "Abaixo do peso";
-        } else if (indice < limiteIdeal) {
+        } else if (indice <= limiteMaximoIdeal) {
             return "Peso Ideal";
-        } else if (indice < limiteLevementeAcimaPeso) {
+        } else if (indice <= limiteLevementeAcimaPeso) {
             return "Acima do peso";
-        } else if (indice < limiteObedisadeGrauI) {
+        } else if (indice <= limiteObedisadeGrauI) {
             return "Obesidade grau I";
-        } else if (indice < limiteObedisadeGrauII) {
+        } else if (indice <= limiteObedisadeGrauII) {
             return "Obesidade grau II";
         } else {
             return "Obesidade grau III";
@@ -36,14 +44,18 @@ public class CalculoImcHelper {
 
     public GravidadeIndiceImc getGravidadeIndice() {
 
-        if (indice < limiteAbaixoPeso) {
+        if (indice < limiteMinimoIdeal) {
             return GravidadeIndiceImc.BAIXA;
-        } else if (indice < limiteIdeal) {
+        } else if (indice <= limiteMaximoIdeal) {
             return GravidadeIndiceImc.NENHUMA;
-        } else if (indice < limiteLevementeAcimaPeso) {
+        } else if (indice <= limiteLevementeAcimaPeso) {
             return GravidadeIndiceImc.BAIXA;
         } else {
             return GravidadeIndiceImc.ALTA;
         }
+    }
+
+    public double getDiferencaPesoParaSituacaoIdeal() {
+        return indice - (altura*altura);
     }
 }
